ee066184d8768a2c3f5ae01a45059a59adc15052
NicolasPavz <npavez43@gmail.com>
2025-07-04 13:41:28 -0400

    reader service

---
+++ src/services/readerService.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API = import.meta.env.VITE_API_URL + "/reader";
+
+export const changeReaderState = async (email, state) =>{
+    try {
+        const response = await axios.post(`${API}/state/${email}`, { state });
+        return response.data;
+    } catch (error) {
+        console.error("Error cambiando el estado del lector", error);
+        throw error;
+    }
+}
+
+export const getReaderByEmail = async (email) => {
+    try {
+        const response = await axios.get(`${API}/find/${email}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error obteniendo lector por email", error);
+        throw error;
+    }
+}
